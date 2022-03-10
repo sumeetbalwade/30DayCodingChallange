0f55b610466f48b248dbe5f7e88e84c88971ded2
Sumeet Balwade <balwadesr@gmail.com>
2022-03-11 00:46:04 +0530

    day23

---
+++ day-23-lot.py
@@ -0,0 +1,35 @@
+import sys
+
+
+class Node:
+    def __init__(self, data):
+        self.right = self.left = None
+        self.data = data
+
+
+class Solution:
+    def insert(self, root, data):
+        if root == None:
+            return Node(data)
+        else:
+            if data <= root.data:
+                cur = self.insert(root.left, data)
+                root.left = cur
+            else:
+                cur = self.insert(root.right, data)
+                root.right = cur
+        return root
+
+    def levelOrder(self, root):
+        queue = [root]
+        while queue:
+            node = queue.pop()
+            print(node.data, end=' ')
+
+            if node.left:
+                queue.insert(False, node.left)
+            if node.right:
+                queue.insert(False, node.right)
+
+
+T = int(input())
